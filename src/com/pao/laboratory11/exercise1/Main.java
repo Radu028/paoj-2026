02e24c523a942db10106a56cab4eee762a2818a8
Ștefan E. Deaconu <stefan@mindliteracy.com>
2026-05-03 14:34:05 +0300

    reqs for lab11

---
+++ src/com/pao/laboratory11/exercise1/Main.java
@@ -0,0 +1,5 @@
+package com.pao.laboratory11.exercise1;
+
+public class Main {
+    // TODO implementati toate cerintele tinand cond de indrumari
+}
---
+++ src/com/pao/laboratory11/exercise2/Main.java
@@ -0,0 +1,6 @@
+package com.pao.laboratory11.exercise2;
+
+public class Main {
+    // TODO puteti implementa toate cerintele direct in Main.java,
+    //  dar cu functii modulare in functie de cum considerati
+}
